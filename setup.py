ce489fd59befd4161a210aea38d5a22f83cae276
chepecarlos <chepecarlos@alswblog.org>
2022-04-04 16:24:10 -0600

    actualiznado algoritmo de instalar

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import find_packages, setup
 with open("VERSION", "r") as f:
     version = f.read().strip()
 
+with open("requirements.txt", "r") as f:
+    required = f.read().splitlines()
 
 with open(file="README.md", mode="r") as readme_handle:
     long_description = readme_handle.read()
@@ -15,7 +17,7 @@ setup(
     author="ChepeCarlos",
     author_email="chepecarlos@alswblog.org",
     url="https://github.com/chepecarlos/tooltube",
-    install_requires=[],
+    install_requires=required,
     packages=find_packages(where="src", exclude=("tests*", "testing*")),
     package_dir={"": "src"},
     entry_points={
