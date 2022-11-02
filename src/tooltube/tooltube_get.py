533ccd3ba621b0adca2b1c401b4f2b8ac19bafec
chepecarlos <chepecarlos@alswblog.org>
2022-11-02 13:44:01 -0600

    agregar buscador de proyectos

--- src/tooltube/tooltube_get.py
+++ src/tooltube/tooltube_get.py
@@ -1,8 +1,10 @@
 import argparse
+import os
 
 import colorama
 
 import tooltube.miLibrerias as miLibrerias
+from tooltube.miLibrerias import ObtenerArchivo
 
 logger = miLibrerias.ConfigurarLogging(__name__)
 import tooltube.funcionesExtras as funcionesExtras
@@ -15,9 +17,28 @@ def ArgumentosCLI():
 
     parser.add_argument("--video_id", "-id", help="ID del video a actualizar Youtube")
     parser.add_argument("--salvar_id", "-s", help="Salvar ID del video")
+    parser.add_argument("--buscar", "-b", help="Buscar un proyecto")
+
     parser.add_argument("-url", help="URL importante del video")
 
     return parser.parse_args()
+
+
+def buscarProyecto(ID):
+    actual = os.getcwd()
+    for base, dirs, files in os.walk(actual):
+        for name in files:
+            if name.endswith(("Info.md")):
+                filepath = base + os.sep + name
+                idArchivo = miLibrerias.ObtenerValor(filepath, "youtube_id")
+                if idArchivo == ID:
+                    filepath = os.path.dirname(filepath)
+                    filepath = os.path.dirname(filepath)
+                    print(f"Encontrado el proyecto {idArchivo} en {filepath}")
+                    print("Abriendo nemo")
+                    os.system(f"nemo {filepath}")
+                    return
+    print(f"No encontrado {ID} :(")
 
 
 def main():
@@ -41,5 +62,8 @@ def main():
             print(f"Video: https://www.youtube.com/watch?v={Video_id}")
             print(f"Editar: https://studio.youtube.com/video/{Video_id}/edit")
             print(f"Analítica: https://studio.youtube.com/video/{Video_id}/analytics/tab-overview/period-default")
+    elif args.buscar:
+        print(f"Buscando {args.buscar}")
+        buscarProyecto(args.buscar)
     else:
         print("No se encontró opción prueba con -h")
